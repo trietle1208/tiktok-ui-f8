e3651f5f6534c07fe12a9055aca2975a7cffbc90
TrietLe <lmtriet1208@gmail.com>
2024-03-09 21:45:06 +0700

    custom global css

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,5 @@
-const { override, useBabelRc } = require('customize-cra');
+const { override } = require('customize-cra');
 const path = require('path');
 module.exports = override(
-    //use BabelRc file
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useBabelRc(),
+
 );

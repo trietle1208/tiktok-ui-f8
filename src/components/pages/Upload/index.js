ba11eb7a3dfbfe419d0a2832c3e864ec1a411380
TrietLe <lmtriet1208@gmail.com>
2024-03-09 22:50:51 +0700

    + Config router & Layout

---
+++ src/components/pages/Upload/index.js
@@ -0,0 +1,11 @@
+function Upload() {
+    return (
+        <div>
+            <div className="content">
+                <h2>Uploads</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Upload;
